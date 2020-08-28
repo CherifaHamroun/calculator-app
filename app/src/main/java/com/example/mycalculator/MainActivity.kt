0c91d3330293bf19c1c3cb6c1c1b74704044ae31
CherifaHamroun <hc_hamroun@esi.dz>
2020-08-28 19:12:28 +0200

    onDecimal Added

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -8,14 +8,26 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric = false
+    var lastDot = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     fun onDigit(view : View){
         tvInput.append((view as Button).text)
+        lastNumeric = true
     }
     fun onClear(view : View){
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+    fun onDecimal(view : View){
+        if (lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
